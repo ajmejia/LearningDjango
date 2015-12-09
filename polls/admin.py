dbf982545654c3ac23c6414507a206757bbc81c4
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2015-12-09 15:16:12 -0430

    customizing the way Question is shown in admin site

--- polls/admin.py
+++ polls/admin.py
@@ -3,8 +3,21 @@ from django.contrib import admin
 # Register your models here.
 #---------------------------------------------------------------
 #- Here I can tell django admin site that my models can have an
-#- admin interface, as follows:
+#- admin interface.
 #---------------------------------------------------------------
 from .models import Question
 
-admin.site.register(Question)
+
+#---------------------------------------------------------------
+#- I can customize what happens in the admin site, in particular
+#- what happens with the models a registered by defining a
+#- class.
+#---------------------------------------------------------------
+class QuestionAdmin(admin.ModelAdmin):
+  fieldsets = [
+               (None,               {'fields': ['question_text']}), #- I can group fields within div tags using this
+                                                                    #- formatting. Can also define collapsed groups
+               ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}), #- as in here.
+              ]
+
+admin.site.register(Question, QuestionAdmin)   #- Note that I had to add both instances of my question.
