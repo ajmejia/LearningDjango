0bd746ed92213f5fd848f93d530187ce60ae1d2b
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2015-12-09 15:41:51 -0430

    choices can be added dynamically from add question page

--- polls/admin.py
+++ polls/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 #- Here I can tell django admin site that my models can have an
 #- admin interface.
 #---------------------------------------------------------------
-from .models import Question
+from .models import Choice, Question
 
 
 #---------------------------------------------------------------
@@ -13,11 +13,19 @@ from .models import Question
 #- what happens with the models a registered by defining a
 #- class.
 #---------------------------------------------------------------
+class ChoiceInline(admin.StackedInline):
+  model = Choice
+  extra = 3                                                         #- This way I can add a bunch of choices at once.
+
 class QuestionAdmin(admin.ModelAdmin):
   fieldsets = [
                (None,               {'fields': ['question_text']}), #- I can group fields within div tags using this
                                                                     #- formatting. Can also define collapsed groups
                ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}), #- as in here.
               ]
+  inlines = [ChoiceInline]                                          #- Plus, the choices will be added/edited in the
+                                                                    #- question page.
+
+#admin.site.register(Choice)
 
 admin.site.register(Question, QuestionAdmin)   #- Note that I had to add both instances of my question.
