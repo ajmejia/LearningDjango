327f41fcf2faab6610942b918e456cecebe8473e
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2015-12-09 22:22:18 -0430

    plugged new (silly) views

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-               url(r"^$", views.index, name="index"),        #- setting the index view url
+               url(r"^$", views.index, name="index"),                  #- Setting the index view url.
+               url(r"^(?P<question_id>[0-9]+)/$", views.detail,
+                   name="detail"),                                     #- This is how the view it's gonna
+                                                                       #- the url of a question detail.
+               url(r"^(?P<question_id>[0-9]+)/results$", views.results,
+                   name="results"),
+               url(r"^(?P<question_id>[0-9]+)/vote$", views.vote,
+                   name="vote"),
               ]
--- pollsite/urls.py
+++ pollsite/urls.py
@@ -5,7 +5,16 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'pollsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    #-----------------------------------------------------------
+    #- the url function takes te following arguments:
+    #-    * regex: regular expression to map againts defined
+    #-             urls.
+    #-    * view: the to which will be called upon the match.
+    #-    + kwargs: arguments passed to the target view.
+    #-    + name: name usde to reference across templates.
+    #-----------------------------------------------------------
 
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls')),                #- The ^polls/ patterns are handled by
+                                                           #- the polls.urls.
     url(r'^admin/', include(admin.site.urls)),
 ]
