53b6746ddf50949d1104824ef9e964caacb1b9e4
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2015-12-10 22:02:30 -0430

    using generic views to avoid repetitive code

--- polls/urls.py
+++ polls/urls.py
@@ -2,15 +2,18 @@
 #- In this file I will define the url patterns of my polls app.
 #---------------------------------------------------------------
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
-               url(r"^$", views.index, name="index"),                  #- Setting the index view url.
-               url(r"^(?P<question_id>[0-9]+)/$", views.detail,
-                   name="detail"),                                     #- This is how the view it's gonna
-                                                                       #- the url of a question detail.
-               url(r"^(?P<question_id>[0-9]+)/results$", views.results,
-                   name="results"),
-               url(r"^(?P<question_id>[0-9]+)/vote$", views.vote,
-                   name="vote"),
+               #url(r"^$", views.index, name="index"),                  #- Setting the index view url.
+               #url(r"^(?P<question_id>[0-9]+)/$", views.detail,
+               #    name="detail"),                                     #- This is how the view it's gonna
+               #                                                        #- the url of a question detail.
+               #url(r"^(?P<question_id>[0-9]+)/results$", views.results,
+               #    name="results"),
+               url(r"^$", views.IndexView.as_view(), name='index'),
+               url(r"^(?P<pk>[0-9]+)/$", views.DetailView.as_view(), name='detail'),
+               url(r"^(?P<pk>[0-9]+)/results/$", views.ResultsView.as_view(), name='results'),
+               url(r"^(?P<question_id>[0-9]+)/vote$", views.vote, name='vote'),
               ]
