edb76251281e92d733657131f6b3fe50dc27e3b4
Alfredo Mejia-Narvaez <mejia@cida.gob.ve>
2015-12-17 20:40:43 -0430

    starting new with a clean database

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -2,11 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
+import django.contrib.auth.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('auth', '0006_require_contenttypes_0002'),
     ]
 
     operations = [
@@ -14,21 +17,37 @@ class Migration(migrations.Migration):
             name='Choice',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('choice_text', models.CharField(max_length=200)),
+                ('choice', models.CharField(max_length=200)),
                 ('votes', models.IntegerField(default=0)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='PollUser',
+            fields=[
+                ('user_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'abstract': False,
+                'verbose_name': 'user',
+                'verbose_name_plural': 'users',
+            },
+            bases=('auth.user',),
+            managers=[
+                (b'objects', django.contrib.auth.models.UserManager()),
             ],
         ),
         migrations.CreateModel(
             name='Question',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('question_text', models.CharField(max_length=200)),
-                ('pub_date', models.DateTimeField(verbose_name=b'date published')),
+                ('question', models.CharField(max_length=200)),
+                ('created_on', models.DateTimeField(verbose_name=b'date published')),
+                ('created_by', models.ForeignKey(to='polls.PollUser')),
             ],
         ),
         migrations.AddField(
             model_name='choice',
-            name='question',
-            field=models.ForeignKey(to='polls.Question'),
+            name='for_question',
+            field=models.ForeignKey(verbose_name=b'choice for question', to='polls.Question'),
         ),
     ]
